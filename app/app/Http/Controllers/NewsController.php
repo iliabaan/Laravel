f8fc0baab01c6b6c1c400b906281af5fa24d0ab7
Ilya Antipenko <52933653+iliabaan@users.noreply.github.com>
2021-05-20 00:06:45 +0300

    hw2

---
+++ app/app/Http/Controllers/NewsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('news.index', [
+            'newsList' => $this->newsList,
+            'newsCategories' => $this->newsCategory
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        return view('news.show', [
+            'news' => $this->newsList[$id]
+        ]);
+    }
+
+    public function by_categories(int $id)
+    {
+        return view('news.by_categories', [
+            'category' => $this->newsCategory[$id],
+            'newsList' => $this->newsList
+        ]);
+    }
+}
